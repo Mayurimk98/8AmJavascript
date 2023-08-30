516306c40f3541cf784f26cbd6d04a7e19450952
Mayurimk98 <mayurimkatwe@gmail.com>
2023-08-30 07:50:42 +0530

    Script1

---
+++ Script1.js
@@ -0,0 +1,62 @@
+// //Javascript
+
+// //printing statement
+// console.log(10)
+
+// console.log("mayuri")
+
+// console.log(10+22)
+
+// //information store
+
+// let x=20
+// //keyword variableName =value
+
+// console.log(x)
+
+// console.log(x)
+
+// console.log(x)
+
+
+// //update
+
+// //variableName= newValue
+// x=50
+// console.log(x)
+// console.log(x)
+// console.log(x)
+
+// const y=10
+// //keyword variableName =value
+
+// console.log(y)
+
+// //we cant do update if we write anything with const keyword
+
+// y=100
+// console.log(y)
+
+
+
+let firstName ="Mayuri"
+console.log(firstName)  //Mayuri
+
+//update
+firstName="Vaibhav"
+console.log(firstName)
+console.log(firstName)
+console.log(firstName)
+
+
+//javascript case sensitive
+
+
+const fruit="apple"
+console.log(fruit) //apple
+
+fruit="banana"   //upadte
+console.log(fruit) //error
+
+
+//comment  ==> user undesrstading
