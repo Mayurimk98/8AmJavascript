58c5a9cc444a9c7d4a5b9f349242535cdaf82184
Mayurimk98 <mayurimkatwe@gmail.com>
2023-08-30 08:34:40 +0530

    script 2

---
+++ Script2.js
@@ -0,0 +1,122 @@
+
+// let fullName = "Mayuri"
+// console.log(fullName)
+// fullName = "gaurav" //update
+// console.log(fullName)
+
+
+// const y1 = 23
+// console.log(y1) //23
+// const y2 = 200
+// console.log(y1) //23
+// y2 = 50
+// console.log(y1) //23
+// console.log(y2) //error
+
+//Operator
+
+//1. arithmatic operator
+//+  addition
+//- subtraction
+//* multiplication
+// / division
+// % modulus  ==> reminder
+
+// let x=10
+// let y=2
+// console.log(x+y) //12
+// console.log(x-y) //8
+// console.log(x*y) //20
+// console.log(x/y) //5
+// console.log(x%y) //0
+
+
+// console.log(19%6) //1
+
+
+// let p=100
+// let q=50
+// console.log(p+q) //150
+// console.log(p-q) //50
+// console.log(p*q)//500
+// console.log(p/q)//2
+// console.log(p%q)//0
+
+//10 pair ==> 50 line  ==> effort , memory, time
+
+//DRY  ==> donot repeat yourself
+
+
+//function
+
+//() => parathesis
+//{} => block
+
+//syntax
+// function functionName(para1, para2){
+// //     //statement
+// // }
+// // function call ==> functionName
+
+
+// //new joiner ==> 3 floor / 3 number tea shop  tea 
+
+
+// // let x1=20
+// // let x2=2
+
+// // function calculator(a,b){
+// //     console.log(a+b)
+// //     console.log(a-b)
+// //     console.log(a*b)
+// //     console.log(a/b)
+// //     console.log(a%b)
+// // }
+// // calculator(20,2)
+
+// // calculator(30,5)
+
+
+// //1. function without para without return type
+
+// function add() {
+//     console.log(20 + 5)
+// }
+
+// add() //25
+// add() //25
+// add() //25
+
+// //o/p => not change
+// //cant use the ouput
+
+// //2. function with para without return type
+// function add1(s1, s2) {
+//     console.log(s1 + s2)
+// }
+// add1(5,2) //7
+// add1(10,10) //20
+// add1(90,10) //100
+
+//o/p =>change
+//cant use the output
+
+//100  => show  => cant use
+//100 => give => 
+
+
+//3. function with para with return type
+function add2(f1, f2) {
+    return f1 + f2
+}
+let sum = add2(7, 10)
+//let sum=17
+console.log(sum)
+
+let s1 = add2(90, 55)
+console.log(s1) //145
+console.log(s1 + 5) //150
+console.log(s1 - 10) //135
+
+
+
