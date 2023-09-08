176151ae384ad07bf28c93d72d88540cf25885d2
Mayurimk98 <mayurimkatwe@gmail.com>
2023-09-08 08:44:47 +0530

    Script8

---
+++ Script8.js
@@ -0,0 +1,141 @@
+
+// //variable
+// //operator
+// // arithmatic
+// //logical
+// //comparison
+// //function
+// //conditional statement
+// // if if else if if else switch case ternary operator
+
+// //loop ==> repetative operator based on condition
+
+// //for loop
+
+// // for(initialization;condition check;incre/decre){
+// //     statements
+// // }
+
+// //welcome ==> 5 time
+
+// for (let i = 0; i < 5; i++) {
+//     console.log("welcome")
+// }
+
+
+// //forward loop
+// //1-5 number
+
+// for (let i = 1; i <= 5; i++) {//2  3 4  5 6
+//     console.log(i) //1  2  3  4  5
+// }
+
+// //i=1
+
+// //1<=5 ==> True
+// //2<=5 ==> true
+// //3<=5 ==> true
+// //4<=5 ==> true
+// //5<=5==> true
+// //6<=5 ==> false
+
+// //===========================================================
+// //reverse loop
+
+
+// //5-0
+
+// for (let i = 5; i >= 0; i--) { //4 3 2 1 0 -1
+//     console.log(i) //5 4 3 2 1 0
+// }
+
+//i=5
+
+//5>=0 ==> true
+//4>=0 ==> true
+//3>=0 ==> true
+//2>=0 ==> true
+//1>=0 ==> true
+//0>=0 ==> true
+//-1>=0 ==> false
+
+
+
+//5 table 
+// 5 10 15 20------50
+
+//5+5 ==> 10
+//10+5 ==> 15
+
+// i++ ==> i=i+1
+//i=i+5
+
+// console.log("=============================")
+// for (let i = 5; i <= 50; i = i + 5) {
+//     console.log(i)
+// }
+
+
+// for (let i = 50; i >= 5; i = i - 5) {
+//     console.log(i)
+// }
+
+
+//1-5 number addition
+//1
+//1+2 => 3
+//3+3 ==> 6
+
+//1  => sum
+//sum=sum+2==> 3
+//sum=sum+3 ==> 6
+//sum=sum+4 ==> 10
+//sum=sum+5 ==> 15
+//sum==> print
+
+
+// let sum = 0 //1  3  6  10 15
+// for (let i = 1; i <= 10; i++) {
+//     //console.log(i) //1 2 3 4 5
+//     sum = sum + i
+// }
+// console.log(sum)
+
+
+//0+1  => 1
+//1+2 => 3
+//3+3=>  6
+//6+4==> 10
+//10+5 ==> 15
+
+
+
+// let x1=10
+// x1=20
+// x1=x1+5
+
+
+
+//1-5 addition
+let x1 = 0 //1  3  6  10  15
+for (let i = 1; i <= 5; i++) { //2  3  4  5  6
+    console.log(i) //1  2  3  4  5
+     x1 = x1 + i  //0+1   1+2   3+3  6+4  10+5
+}
+
+console.log(x1)
+
+
+
+//x1=0
+
+//i=1
+
+//i<=5
+//2<=5
+//3<=5 
+//4<=5
+//5<=5
+//6<=5
+
+
