ca9a0ae76ca7136b1e2cf34caa071d825c950b78
Mayurimk98 <mayurimkatwe@gmail.com>
2023-09-13 08:42:31 +0530

    script11

---
+++ Script11.js
@@ -0,0 +1,127 @@
+
+// //for loop
+
+
+// //welcome ==> 10 times
+// for (let i = 0; i < 10; i++) {
+//     console.log("Welcome")
+// }
+
+
+// //20 ==> 40
+// for (let i = 20; i <= 40; i++) {
+//     console.log(i)
+// }
+
+
+// //11 table
+
+// // for (let i = 11; i <= 110; i = i + 11) {
+// //     console.log(i)
+// // }
+
+
+// // //10-20 number print --> 15 break
+
+// // for (let i = 10; i <= 20; i++) {
+// //     if (i == 15) {
+// //         break
+// //     }
+// //     console.log(i)
+// // }
+
+
+// // //10 -20==>  15 skip
+
+// // for (let i = 10; i <= 20; i++) {
+// //     if (i == 15) {
+// //         continue
+// //     }
+// //     console.log(i)
+// // }
+
+// //while loop
+
+// //1-10
+
+// let i = 1
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+// //20 table 
+// let j = 20
+// while (j <= 200) {
+//     console.log(j)
+//     j = j + 20
+// }
+
+
+// //hello 3 time
+// let k = 1
+// while (k <= 3) {
+//     console.log("hello")
+//     k++
+// }
+
+
+// //100-90
+// let r = 100
+// while (r >= 90) {
+//     console.log(r)
+//     r--
+// }
+
+
+//10-20 ==> 15 break
+// let i = 10
+// while (i <= 20) {
+//     if (i == 15) {
+//         break
+//     }
+//     console.log(i)
+//     i++
+// }
+
+//10-20 ==> 15 skip
+// let j = 10
+// while (j <= 20) {
+//     if (j == 15) {
+//         j++
+//         continue
+//     }
+//     console.log(j)
+//     j++
+// }
+
+//==================================================================
+
+
+let a = 20
+console.log(a) //20
+console.log(typeof a) //number
+
+let b = "true"
+console.log(typeof b) //string
+
+let c = false
+console.log(typeof c) //boolean
+
+
+// 20,30,40,50
+// let a1=20,30,40,50
+
+//array ==> datatype
+
+//object
+//sbi , hdfc, png ==> bank
+//methods
+//property
+//array ==> object
+
+//[] ==> collection of multiple element
+let arr = [20, 30, 40, 50, "mayuri", true, undefined]
+
+
