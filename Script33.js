ed463796dbdfe7c60207bc68b7ec60757bf5f0f4
Mayurimk98 <mayurimkatwe@gmail.com>
2023-10-16 09:04:21 +0530

    dom1

--- Script33.js
+++ Script33.js
@@ -43,5 +43,43 @@ let s2 = new Set([66, 88, 99, 44, 66, 88])
 //[66,88,99,44]
 console.log(s2.size)
 
+let mySetA = new Set()
+console.log(mySetA)
+
+mySetA.add(10)
+mySetA.add(20)
+mySetA.add(30)
+mySetA.add(10)
+console.log(mySetA)
+
+let setC = new Set([11, 22, 33, 44])
+console.log(setC)
+
+// //has()
+// console.log(setC.has(11))
+
+// //delete()
+// setC.delete(22)
+// console.log(setC)
+
+// //clear()
+// setC.clear()
+// console.log(setC)
+
+//forEach()
+setC.forEach(function (el) {
+    console.log(el)
+})
+
+let arr = [22, 33, 22, 55, 77, 99, 22]
+//[22,33,55,77,99]
+
+let setD = new Set(arr)
+console.log(setD)
+
+console.log(Array.from(setD))
+
+
+
 
 
