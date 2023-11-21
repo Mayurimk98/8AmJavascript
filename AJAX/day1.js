2c33f9a0a4ad43077517dddfddedd6f2d089fae0
Mayurimk98 <mayurimkatwe@gmail.com>
2023-11-21 09:08:16 +0530

    21 11

---
+++ AJAX/day1.js
@@ -0,0 +1,105 @@
+
+//AJAX==> Asynchronous javascript ==> 
+
+//Synchronous js ==> line to line
+
+
+// function add1(){
+//     console.log("I am add one function")
+// }
+
+// function add2(){
+//     console.log("I am add two function")
+// }
+
+
+// add1()
+// add2()
+
+//I am add two function
+//I am add one function
+
+//setTimout()
+
+//asynch
+
+// function add3() {
+//     setTimeout(function () {
+//         console.log("I am add 3")
+//     }, 2000)
+// }
+
+// function add4() {
+//     console.log('I am add4')
+// }
+
+// add3()//2 sec 
+// add4()
+
+//i am add 4
+//2 sec
+//i am add 3
+
+
+//User create==> get ID==> user info
+
+//asynch => synch
+
+// function getUser() {
+//     setTimeout(function () {
+//         console.log("User Create")
+//     }, 4000)
+
+//     setTimeout(function () {
+//         console.log("Get ID from user create")
+//     }, 3000)
+
+//     setTimeout(function () {
+//         console.log('Get info from id')
+//     }, 2000)
+
+// }
+// getUser()
+// 2sec
+//Get info from id
+//3 sec
+//Get ID from user create
+//4 sec
+//User Create
+
+
+//Asynch ==> synch
+
+//1. call Back hell
+//2. promises
+
+
+function getUser() {
+    setTimeout(function () {
+        console.log("User Created")
+
+        setTimeout(function () {
+            console.log("Get ID")
+
+            setTimeout(function () {
+                console.log("Get Info")
+            }, 1000)
+
+        }, 2000)
+
+    }, 3000)
+}
+getUser()
+
+//reused
+//tightly couple
+
+//ES6
+
+
+
+
+
+
+
+
