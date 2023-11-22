cd3dc5307f95c35cefbbf2d7f1117d97b690dc93
Mayurimk98 <mayurimkatwe@gmail.com>
2023-11-22 08:26:19 +0530

    22 11

---
+++ AJAX/day2.js
@@ -0,0 +1,117 @@
+
+//synch
+
+//asych => setTimeout
+
+//userCreate==> getID=> user Info
+
+//callBackHell
+
+function getInfo() {
+    setTimeout(function () {
+        console.log("User Crate")
+
+        setTimeout(function () {
+            console.log("Get ID")
+
+            setTimeout(function () {
+                console.log("Get User Info")
+            }, 1000)
+        }, 2000)
+    }, 3000)
+}
+
+
+//Promises  => asynch => synch
+
+//pending , resolve,reject
+
+//500 
+
+// let pro = new Promise(function (resolve, reject) {
+//     let x = 50
+//     let y = 50
+
+//     if (x == y) {
+//         resolve("Passed")
+//     }
+//     else {
+//         reject("Failed")
+//     }
+// })
+
+// //consumed
+
+// pro.then(function (str) {
+//     console.log(str)
+// }, function (str) {
+//     console.log(str)
+// })
+
+
+// let pro = new Promise(function (resolve, reject) {
+//     let x = 50
+//     let y = 100
+
+//     if (x == y) {
+//         resolve([40,23,56,78])
+//     }
+//     else {
+//         reject([-40,-23,-56,78])
+//     }
+// })
+
+// //consumed
+
+// pro.then(function (arr) {
+//     console.log(arr[3])//78
+// }, function (arr) {
+//     console.log(arr[2])//-56
+// })
+
+
+// let pro = new Promise(function (resolve, reject) {
+//     let x = 50
+//     let y = 100
+
+//     if (x == y) {
+//         resolve("Pass")
+//     }
+//     else {
+//         reject("fail")
+//     }
+// })
+
+// //consumed
+
+// pro.then(function(str){
+//     console.log(str) //resolve
+// }).catch(function(str){
+//     console.log(str)//reject
+// })
+
+
+
+let pro = new Promise(function (resolve, reject) {
+    let x = 50
+    let y = 50
+
+    if (x == y) {
+        resolve("Pass")
+    }
+    else {
+        reject("fail")
+    }
+})
+
+//consumed
+
+pro.then(function(str){
+    console.log(str) //resolve
+}).catch(function(str){
+    console.log(str)//reject
+}).finally(function(){
+    console.log(" I will execute in any case")
+})
+
+
